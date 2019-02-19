1315820b53cf401595a3a0f041b03b50593f2685
Rafael Paixão <rafaelpx8812@gmail.com>
2019-02-19 13:10:49 -0300

    Add files via upload

--- MAE.py
+++ MAE.py
@@ -1,27 +1,41 @@
-import glob
 import numpy as np
 import cv2 as cv
+import glob
+import shutil
+import json
 
+
+dst = 'D:/Documentos/Lego/repetidas'
+src = "D:/Documentos/Lego/imagens/*.jpg"
+data ={}
 
 def calculateMAE(imgA, imgB):
 
-    mae = np.sum(np.absolute((imgB.astype("float")-imgA.astype("float"))))
-    mae /= float(imgA.shape[0] * imgA.shape[1]*255)
-    if mae < 0:
-        return mae * -1
-    else:
-        return mae
+    mae = np.sum(np.absolute(imgB.astype("float")-imgA.astype("float")))
+    mae /= float(imgA.shape[0] * imgA.shape[1])
+
+    return abs(mae)
 
 
-img = cv.imread("images/img.jpg")
-imgteste = cv.imread("images/imgteste.jpg")
-img = cv.resize(img,(300, 300))
-imgteste = cv.resize(imgteste, (300, 300))
-cv.imshow("img", img)
-cv.imshow("teste", imgteste)
+tree = glob.glob(src)
+print(tree)
 
-print(calculateMAE(img, imgteste))
+for filename in tree:
+    imgA = cv.imread(filename)
+    imgA = cv.resize(imgA, (300, 300))
+    for i,fl in enumerate(glob.glob(src)):
+        imgB = cv.imread(fl)
+        imgB = cv.resize(imgB, (300, 300))
+        if filename != fl:
+            mae = calculateMAE(imgA, imgB)
+            if (mae >= 0.0 and mae <=0.5):
+                shutil.move(fl, dst)
+                data.setdefault(filename, []).append(fl)
+                tree.remove(fl)
 
-cv.waitKey(0)
-cv.destroyAllWindows()
 
+
+log = json.dumps(data)
+print(log)
+with open('log.json', 'w') as outfile:
+    json.dump(log, outfile)
